579529367e5e5b257ca9eedc717c56f0b44a663b
Nadrieril <nadrieril@gmail.com>
2015-12-13 15:54:52 +0100

    Add error reporting

--- merge_courses.py
+++ merge_courses.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import yaml, json, sys, os
 
 COURSES_DIR = 'data/courses'
@@ -18,6 +20,34 @@ def make_courses_list(courses_list, courses_data):
                 ret.append(o)
 
     return ret
+
+def compute_errors(courses):
+    errors = []
+    for c in l:
+        def err(b, s): errors.append({'id': c['id'], 'msg': s, 'err': b})
+
+        hasContent = 'content' in c
+        if not hasContent:
+            err(True, "Le cours n'a pas de description")
+        else:
+            n = len(c['content'])
+            if n > 500:
+                err(True, "La description fait plus de 500 caractères (%d)" % n)
+
+        hasHours = 'hoursLectures' in c or 'hoursPracticalWork' in c or 'hoursTutorial' in c
+        if not hasHours:
+            err(True, "Le nombre d'heures par semaine n'est pas indiqué")
+
+        hasWeeks = 'weeks' in c
+        if not hasWeeks:
+            err(True, "Le nombre de semaines n'est pas indiqué")
+
+        hasCredits = 'credits' in c
+        if not hasCredits:
+            err(False, "Le nombre de crédits n'est pas indiqué")
+
+    return errors
+
 
 def display_status(courses):
     def cut(s, n): return ("{:"+str(n)+"}").format(s)[:n]
@@ -65,7 +95,11 @@ for dirname, dirnames, filenames in os.walk(COURSES_DIR):
                 courses_data.update(o)
 
 l = make_courses_list(courses_list, courses_data)
-display_status(l)
+# display_status(l)
+
+for x in compute_errors(l):
+    err = "Err " if x['err'] else "Warn"
+    print("[%s] %s: %s" % (err, x['id'], x['msg']))
 
 with open("merged.yml", "w") as f:
     yaml.safe_dump({'courses':l}, f)
